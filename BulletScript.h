60811fb3be37b546906f195acf33015b9844e4ab
Chanwoongs <tkfkdcksdnd@daum.net>
2021-12-17 09:47:33 +0900

    초기 파일 업로드
    -헬리콥터 움직임 구현 완료
    
    -헬리콥터 총 발사 구현중

---
+++ BulletScript.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Behaviour.h"
+#include "Transform.h"
+class BulletScript :
+    public Behaviour
+{
+private:
+public:
+    BulletScript(GameObject* gameObject) : Behaviour(gameObject)
+    {}
+
+    void update() override
+    {
+        auto pos = transform->getPos();
+        transform->setPos(pos + Position::right);
+        
+        //destroy(gameObject);
+    }
+};
+
